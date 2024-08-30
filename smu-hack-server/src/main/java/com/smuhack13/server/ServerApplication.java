011d5234e6001768fbac3eaf2cd049dba9b38b9c
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-08-31 00:04:48 +0900

    initial commit

---
+++ smu-hack-server/src/main/java/com/smuhack13/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.smuhack13.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
