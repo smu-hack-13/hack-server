6962aa779a005523989d2b06d1166b39a629feb3
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-08-31 09:48:26 +0900

    feat : prompt 추가

--- smu-hack-server/src/main/java/com/smuhack13/server/api/service/GptPromptService.java
+++ smu-hack-server/src/main/java/com/smuhack13/server/api/service/GptPromptService.java
@@ -35,10 +35,30 @@ public class GptPromptService {
             String pdfText = s3PdfResponse.text();
             String truncatedText = pdfText.substring(0, Math.min(1000, pdfText.length()));
 
-            String prompt = "Using the following regulations for " + country + ":\n\n"
-                    + truncatedText + "\n\n"
-                    + "Generate an HTML file based on the following user input: "
-                    + userInput;
+//            String prompt = "Using the following regulations for " + country + ":\n\n"
+//                    + truncatedText + "\n\n"
+//                    + "Generate an HTML file based on the following user input: "
+//                    + userInput;
+
+            // input_information에 사용자 입력 추가
+            String input_information = "a";
+
+            String request_information = String.format("입력된 영양정보는 영양성분명과 함량, 1일 영양성분 기준치에 대한 비율을 각 영양성분마다 나타낸것입니다.\n" +
+                            "아래의 요청사항들을 모두 철저히 준수해서 %s의 %s영양성분 표기 규정에 맞게 작성된 영양성분표를 웹에서 바로 확인할 수 있도록 HTML 코드를 작성해 주세요.\n" +
+                            "요청사항:\n" +
+                            "1. 영양성분 나열 순서 확인: %s의 %s영양성분 표기 규정에 따라 입력된 영양성분명을 순서에 맞게 올바르게 배치합니다.\n" +
+                            "2. 추가 문구 확인: %s의 %s영양성분 표기 규정에 따라 추가적으로 필요한 문구가 있는지 확인합니다.\n" +
+                            "3. 필요 없는 항목 확인: %s의 %s영양성분 표기 규정에 따라 영양성분 함량과 1일 영양성분 기준치 비율 중에서 규정상 필요 없는 항목이 있는지 확인합니다.\n" +
+                            "4. 요소의 굵기 및 크기 확인: %s의 %s영양성분 표기 규정에 따라 각 요소가 영양성분인지, 함량인지, 1일 영양성분 기준치에 대한 비율인지 확인하고 종류별로 규정에 따른 적절한 글씨 굵기와 크기를 확인합니다.\n" +
+                            "5. 라인 개수 확인: %s의 %s영양성분 표기 규정에 따라 영양성분표에 포함되어야 하는 라인의 수를 확인합니다.\n" +
+                            "6. 라인 굵기 확인: %s의 %s영양성분 표기 규정에 따라 영양성분표에 포함되는 라인의 종류를 확인하고 그에 맞는 적절한 굵기를 확인합니다.\n" +
+                            "7. 요소 간 거리 확인: %s의 %s영양성분 표기 규정에 따라 각 요소가 영양성분인지, 함량인지, 1일 영양성분 기준치에 대한 비율인지 확인하고 그에 따른 요소 간 올바른 거리를 확인합니다.",
+                    country, type, country, type, country, type, country, type, country, type, country, type, country, type, country, type);
+
+            String prompt = input_information + "\n" + request_information;
+
+
+
 
             return gptService.generateHtml(prompt);
 
